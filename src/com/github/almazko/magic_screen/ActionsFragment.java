568ae730f6bd895522921739b55ed91f14ad38b2
Almazko <a.s.suslov@gmail.com>
2014-06-14 14:12:52 +0400

    #2 add border. clean layouts

--- src/com/github/almazko/magic_screen/ActionsFragment.java
+++ src/com/github/almazko/magic_screen/ActionsFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.github.almazko.magic_screen.MyActivity.Stage;
 
 /**
